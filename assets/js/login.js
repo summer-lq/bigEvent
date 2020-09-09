f37f73097382f2e6e0233386ba5837f21e6a0154
summer-lq <w2681407905@gmail.com>
2020-09-09 17:08:13 +0800

    添加的表单的校验规则

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,5 @@
 $(function() {
+    //登录和主页的相互切换
     $('#link_reg').on('click', function() {
         $('.login-box').hide()
         $('.reg-box').show()
@@ -6,5 +7,20 @@ $(function() {
     $('#link_login').on('click', function() {
         $('.login-box').show()
         $('.reg-box').hide()
+    })
+
+    //自定义校验规则
+    var form = layui.form
+    form.verify({
+        //校验密码的规则
+        password: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        //校验两次密码是否一致的规则
+        repassword: function(value) {
+            //通过属性选择器可以获取密码框的值
+            var pwd = $('.reg-box[name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致'
+            }
+        }
     })
 })
