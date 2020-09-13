a86f6548f3d5c91f9647b6995414ac8590ac8023
summer-lq <w2681407905@gmail.com>
2020-09-13 20:12:27 +0800

    修饰

--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -2,19 +2,20 @@ $(function() {
     //密码校验规则
     var form = layui.form
     form.verify({
-            pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-            samePwd: function(value) {
-                if (value === $('.layui-form [name=oldPwd]').val()) {
-                    return '新密码和旧密码不能一致'
-                }
-            },
-            rePwd: function(value) {
-                if (value !== $('.layui-form [name=newPwd]').val()) {
-                    return '确认密码与新密码不一致'
-                }
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        samePwd: function(value) {
+            if (value === $('.layui-form [name=oldPwd]').val()) {
+                return '新密码和旧密码不能一致'
             }
-        })
-        //重置密码操作
+        },
+        rePwd: function(value) {
+            if (value !== $('.layui-form [name=newPwd]').val()) {
+                return '确认密码与新密码不一致'
+            }
+        }
+    })
+
+    //重置密码操作
     $('.layui-form').submit('on', function(e) {
         e.preventDefault()
         $.ajax({
