f0a89326e697105a870ee5d459b3460656da478f
summer-lq <w2681407905@gmail.com>
2020-09-15 10:39:07 +0800

    分页功能完成

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -2,6 +2,7 @@ $(function() {
 
     var layer = layui.layer
     var form = layui.form
+    var laypage = layui.laypage
 
     //声明配置参数对象
     var q = {
@@ -28,11 +29,12 @@ $(function() {
                 })
                 var tableHtml = template('tpl-table', res)
                 $('tbody').html(tableHtml)
+                renderPage(res.total)
             }
         })
     }
 
-    //获取下拉列表项
+    //动态生成下拉菜单项
     initCate()
 
     function initCate() {
@@ -60,5 +62,31 @@ $(function() {
         q.state = newstate
         initTable()
     })
+
+
+
+    //分页模块
+    function renderPage(total) {
+        laypage.render({
+            elem: 'renderpage',
+            count: total,
+            limit: q.pagesize,
+            curr: q.pagenum,
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+            limits: [2, 3, 5, 10],
+            //触发jump方法有两种：
+            //1.第一次加载，会触发jump方法
+            //2.当点击页码的时候会触发jump方法
+            jump: function(obj, first) {
+                q.pagenum = obj.curr
+                q.pagesize = obj.limit
+
+                //当第一次加载的时候，first的值为true,当点击页码值得时候,first的值为undefined
+                if (!first) {
+                    initTable()
+                }
+            }
+        })
+    }
 
 })
